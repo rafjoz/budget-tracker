879a2141f0d1414a7a038f6c5e0d6a547fdafb2b
rafjoz <rafay@supportingorphans.org>
2026-06-07 16:11:56 -0400

    spendie-v3.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spendie-v3.2.1.1';
+const CACHE_NAME = 'spendie-v3.3';
 const ASSETS = ['./', './index.html', './manifest.json', './Icon.png', './Logo.png'];
 
 // ── INSTALL ──────────────────────────────────────────────────
